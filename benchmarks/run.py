27600430b36ebaf2644cdfd46e78941cdcc86df1
codr7 <codr7@protonmail.com>
2025-06-08 17:19:33 +0200

    Add fact benchmark.

--- benchmarks/run.py
+++ benchmarks/run.py
@@ -4,7 +4,13 @@ def benchmark(reps, setup, test):
     Timer(test, setup).timeit(reps)
     return Timer(test, setup).timeit(reps)
 
-print(benchmark(1000, '''
+print('fact', benchmark(500000, '''
+def fact(acc, n):
+  return acc if n == 1 else fact(n * acc, n - 1)
+''',
+'fact(1, 15)'))
+
+print('fib', benchmark(500, '''
 def fib(n):
   return n if n < 2 else fib(n - 1) + fib(n - 2)
 ''', 'fib(20)'))
